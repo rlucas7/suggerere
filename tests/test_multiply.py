9f81d5464baaa509e38b7b1897ad7758ca6a6a9a
Lucas Roberts <rlucas7@vt.edu>
2025-02-26 22:12:07 -0500

    Update tests/test_multiply.py

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -6,8 +6,7 @@ import pytest
 #from unittest import TestCase
 
 
-
-class TestMultiply:
+ # some comment here to commit
     # you can use the parametrize fixture for concise looping like so
     @pytest.mark.parametrize("a,b,expected", [(3,5,15), (2,4,8)])
     def test_multiply(self, a, b, expected):
