90b82e3b2a91bb3f9f88a2ac4ca33d557027d71a
Lucas Roberts <rlucas@Lucass-MacBook.lan>
2025-02-26 21:14:26 -0500

    add a multiply function
      * this is simply to create an example PR so that I can
      confirm the post api for gh to post comments/suggestions
      via rest api for gh pr

---
+++ tests/test_multiply.py
@@ -0,0 +1,15 @@
+from collections2.multiply import multiply
+
+import pytest
+
+# uncomment if needed
+#from unittest import TestCase
+
+
+
+class TestMultiply:
+    # you can use the parametrize fixture for concise looping like so
+    @pytest.mark.parametrize("a,b,expected", [(3,5,15), (2,4,8)])
+    def test_multiply(self, a, b, expected):
+        assert multiply(a, b) == expected
+
